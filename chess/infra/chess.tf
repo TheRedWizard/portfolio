907f6bcbfa44c11751ee7c8b272fc537f616b613
= <=>
2018-08-26 13:50:11 -0700

    added a basic index.html and got terraform copying over files, will switch to a git checkout now

--- chess/infra/chess.tf
+++ chess/infra/chess.tf
@@ -7,6 +7,14 @@ resource "aws_security_group" "app" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0 
     to_port = 65535
@@ -20,8 +28,29 @@ resource "aws_instance" "app" {
   instance_type = "t2.nano"
   key_name = "${var.keyname}"
   security_groups = ["${aws_security_group.app.name}"]
+
+  provisioner "file" {
+    source = "client/src/index.html"
+    destination = "/tmp/index.html"
+    connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = "${file("/Users/ericcrook/pems/eric-dev.pem")}"
+    }
+  }
+
+
   provisioner "remote-exec" {
-  inline = ["sudo yum install git -y"]
+
+  inline = [
+    "sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm",
+    "sudo yum update -y",
+    "sudo yum install git -y",
+    "sudo yum install nginx -y",
+    "sudo systemctl start nginx",
+    "sudo rm -rf /usr/share/nginx/html/*",
+    "sudo cp /tmp/index.html /usr/share/nginx/html/index.html"
+  ]
 
     connection {
       type = "ssh"
